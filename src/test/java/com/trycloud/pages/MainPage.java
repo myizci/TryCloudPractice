ba8b91037a43bfce43d08a439345661379ba4774
Dragisa <gisapetrovic83@gmail.com>
2021-02-06 12:46:01 -0500

    Decorating my US7

--- src/test/java/com/trycloud/pages/MainPage.java
+++ src/test/java/com/trycloud/pages/MainPage.java
@@ -37,8 +37,11 @@ public class MainPage extends BasePage {
     public String expectedCalendarTitle = " - Calendar - Trycloud QA";
     public String expectedDeckTitle = "Deck - Trycloud QA";
 
-    //public  WebElement actualModuleName  = driver.findElement(By.xpath("//a[contains(.,'All files')]"));
+
     //US3_7
-
-
+    public String actionIconXpath = "(//a[@class='action action-menu permanent'])[2]";
+    public String detailsXpath = "//span[.='Details']";
+    public String commentFolderXpath = "//a[@id='commentsTabView']";
+    public String commentBoxXpath = "(//div[@contenteditable='true'])[2]";
+    public String submitButtonXpath = "//input[@class='submit icon-confirm has-tooltip']";
 }
--- src/test/java/com/trycloud/tests/US3_TC7.java
+++ src/test/java/com/trycloud/tests/US3_TC7.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class US3_TC7 extends TestBase {
 
     //WebElement uploadFile = driver.findElement(By.xpath("//label[@for='file_upload_start']"));
 
     @Test
-    public void test_UploadingAFile(){
+    public void test_UploadingAFile() {
         //1. Login as a user: login is included in the TestBase.
 
         //Creating a mainPage Object
@@ -23,18 +26,18 @@ public class US3_TC7 extends TestBase {
         driver.findElement(By.xpath(mainPage.filesXpath)).click();
 
         //2. Choose a folder from the page(clicking on File folder)
-       BrowserUtils.sleep(4);
-       //this is a table, so this should be correct xpath. Try with team again(1/2)
-        driver.findElement(By.xpath("//table[@class='list-container  has-controls']//tr[2]//td[2]")).click();
+        BrowserUtils.sleep(4);
+        //this is a table, so this should be correct xpath. Try with team again(1/2)
+        driver.findElement(By.xpath("(//span[.='Petra'])[3]")).click();
 
         //3. Click the “+” icon on top
-       driver.findElement(By.xpath(mainPage.plusButtonXpath)).click();
+        driver.findElement(By.xpath(mainPage.plusButtonXpath)).click();
 
-       //4. Click “upload file”
+        //4. Click “upload file”
         WebElement uploadingButton = driver.findElement(By.xpath(mainPage.addFileButtonXpath));
 
         //Creating a path of the file
-        String path = "\u202AC:\\Users\\gisa_\\Desktop\\Screenshot 2020-12-29 204944.png";
+        String path = "\"C:\\Users\\gisa_\\Desktop\\Screenshot 2020-12-28 212655.png\"";
         //5. Upload a file
         BrowserUtils.sleep(2);
         uploadingButton.sendKeys(path);
@@ -46,6 +49,7 @@ public class US3_TC7 extends TestBase {
 //        //(Pre-condition: there should be at least 1 folder is created on the filers page)
 //        //Test case #7 - verify users can upload a file inside a folder
     }
+
     @Test
     public void test_writingAComment() {
 
@@ -55,21 +59,35 @@ public class US3_TC7 extends TestBase {
         driver.findElement(By.xpath(mainPage.filesXpath)).click();
         BrowserUtils.sleep(2);
         //2. Click action-icon from any file on the page
-        driver.findElement(By.xpath("(//a[@class='action action-menu permanent'])[2]")).click();
+        driver.findElement(By.xpath(mainPage.actionIconXpath)).click();
         //3. Click details
         BrowserUtils.sleep(2);
-        driver.findElement(By.xpath("//span[.='Details']")).click();
-        //Locatin a comment sign
+        driver.findElement(By.xpath(mainPage.detailsXpath)).click();
+        //Locating a comment sign
         BrowserUtils.sleep(2);
-        driver.findElement(By.xpath("//a[@id='commentsTabView']")).click();
+        driver.findElement(By.xpath(mainPage.commentFolderXpath)).click();
         //4. Write a comment inside to the input box
-        WebElement commentBox = driver.findElement(By.xpath("(//div[@contenteditable='true'])[2]"));
+        WebElement commentBox = driver.findElement(By.xpath(mainPage.commentBoxXpath));
         commentBox.clear();
         commentBox.sendKeys("Excellent job!");
         //5. Click the submit button to post it
         BrowserUtils.sleep(2);
-        driver.findElement(By.xpath("//input[@class='submit icon-confirm has-tooltip']")).click();
+        driver.findElement(By.xpath(mainPage.submitButtonXpath)).click();
         //6. Verify the comment is displayed in the comment section.
 
+        boolean commentFlag = true;
+        List<WebElement> listOfComments = new ArrayList<>();
+
+        for (WebElement each : listOfComments) {
+            if (each.getText().equals("Excellent job!")) {
+                System.out.println(each.getText());
+                commentFlag = false;
+                break;
+            }
+            if (commentFlag) {
+                System.out.println("Test failed!");
+            }
+
+        }
     }
 }
