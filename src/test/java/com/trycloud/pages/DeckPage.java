a03c2844e33982a5deb3ca4526623a794ab865bd
Sladjan1983 <74144698+Sladjan1983@users.noreply.github.com>
2021-02-09 20:30:31 -0500

    Final work

---
+++ src/test/java/com/trycloud/pages/DeckPage.java
@@ -0,0 +1,11 @@
+package com.trycloud.pages;
+
+public class DeckPage extends BasePage{
+    public final String toggleMenu = "//a[@class='app-navigation-toggle']";
+    public final String plusButton = "//div[@class='app-navigation-entry-icon icon-add']";
+    public final String boardLocation ="//input[@placeholder='Board name']";
+    public final String boardList = "//ul[@class='app-navigation-entry__children']//span";
+}
+
+
+
